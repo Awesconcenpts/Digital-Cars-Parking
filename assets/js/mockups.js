6459c88538738b730c48a4abe8684640bfc274e4
bhusalksb <bhusal.ksb@gmail.com>
2016-12-03 08:21:30 +0545

    latest changes

--- assets/js/mockups.js
+++ assets/js/mockups.js
@@ -2,7 +2,10 @@ var Mockups={
     init:function(){
         /* any JavaScript Code will go here */
         jQuery(window).load(function(){
-            jQuery("body").delay(1500).addClass('loaded');
+            setTimeout(function(){
+              jQuery("body").delay(1500).addClass('loaded');  
+            },500)
+            
         })
     }
 }
